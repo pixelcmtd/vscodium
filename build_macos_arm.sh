487802708cf22f0627d8b9cb7932b48f31438183
pixel <chrissx@chrissx.de>
2021-12-06 00:49:38 +0100

    a lot of adjustments to the script

--- build_macos_arm.sh
+++ build_macos_arm.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
+[ $# -lt 1 ] && echo "No version given." && exit 1
+set -uxe
 
-echo "### FETCHING TAGS ###"
-tag_fork=$(git tag -l | tail -n 1)
-cd vscodium || exit
-tag_upstream=$(git tag -l | tail -n 1)
+rm -rf VSCode* vscode
+. get_repo.sh
+SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=arm64 . build.sh
+cd ..
+tar cJf vscodium/VSCode-darwin-arm64/VSCodium.tar.xz vscodium/VSCode-darwin-arm64/VSCodium.app -x "*.DS_Store"
+gpg --armor --sign --local-user C6917CCF69E560DEFD0EFB17E90005837ECF00FF vscodium/VSCode-darwin-arm64/VSCodium.tar.xz
 
-if [[ "${tag_upstream}.darwin_arm64" != "${tag_fork}" ]]; then
-  tag="${tag_upstream}.darwin_arm64"
+gh release create "$1" --notes "A new release that you can actually trust. 😉
 
-  echo "### COMPILING ###"
-  rm -rf VSCode*
-  rm -rf vscode
-  . get_repo.sh
-  SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=arm64 . build.sh
-  cd .. || exit
+🏳️‍⚧️"
+gh release upload "$1" vscodium/VSCode-darwin-arm64/VSCodium.zip
 
-  echo "### CREATING RELEASE ###"
-  gh release create "${tag}" --notes "Added Darwin ARM64 binary for M1 Macs.
-  Compiled on my M1 Macbook Air in just about 5 minutes 🥳"
-  zip -r -X -y vscodium/VSCode-darwin-arm64/VSCodium.zip vscodium/VSCode-darwin-arm64/VSCodium.app -x "*.DS_Store"
-  gh release upload "${tag}" vscodium/VSCode-darwin-arm64/VSCodium.zip
-
-  # code below does not work, don't know why yet
-  #echo "### INSTALLING ###"
-  #sudo cp -r vscodium/VSCode-darwin-arm64/VSCodium.app /Applications/VSCodium.app
-  #sudo xattr -r -d com.apple.quarantine /Applications/VSCodium.app
-else
-  echo "### ALREADY ON LATEST VERSION ###"
-fi
+sudo rm -rf /Applications/VSCodium.app
+sudo cp -r vscodium/VSCode-darwin-arm64/VSCodium.app /Applications/VSCodium.app
+sudo xattr -r -d com.apple.quarantine /Applications/VSCodium.app
